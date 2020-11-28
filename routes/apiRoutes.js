86a8ea560ae7289a29c5338ab8b48a46650a520b
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2020-11-28 08:30:04 -0800

    db loadNotes added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,22 @@
 
 
 const router = require('express').Router();
-const { notes } = require('../db/db.json');
 const { v4: uuidv4 } = require('uuid');
-const syncNotes = require('../lib/notes.js');
-
+const {loadNotes, syncNotes} = require('../lib/notes.js');
 
 
 router.get('/notes', (req,res) =>{
-    console.log('get')
-    console.log(notes);
+    notes = loadNotes();
+    //console.log('get');
+    //console.log(notes);
     res.json(notes);
 });
 
 router.post('/notes', (req,res) =>{
     const newNote = req.body
     newNote.id = uuidv4(); // add id to the package
-    console.log('posted')
-    console.log(newNote);
+    //console.log('posted')
+    //console.log(newNote);
     notes.push(newNote);
     syncNotes(notes)
     res.json(newNote);
